a7a9e2b0f03324b4621cab5a4e60a774c5a63d6e
Chris Penner <christopher.penner@gmail.com>
2019-09-10 16:59:22 -0600

    Remove old site

--- site/src/components/MailingListForm.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import mailingListForm from '../html/mailing-list-form.html'
-
-const MailingListForm = () => <div dangerouslySetInnerHTML={{__html: mailingListForm}}></div>
-
-export default MailingListForm
--- site/src/components/PatreonButton.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import PatreonButtonImg from '../assets/patreon-button.png'
-export default () => <a  href="https://www.patreon.com/bePatron?u=7263362">
-    <img width="170" height="40" src={PatreonButtonImg} alt="Become a Patron!"/>
-</a>
